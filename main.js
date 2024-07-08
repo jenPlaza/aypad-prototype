77a1b5aab88d229558a84f96d25cb48f362c2dfe
Jennifer Plaza <jpplaza@student.fullsail.edu>
2024-07-08 13:12:39 -0400

    modified logo, added gallery html, images, and css

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-alert('js file');
+//alert('js file');
 //Navigation Menu onclick functionality for mobile sizes
 //function appear() {
-  //alert('appear');
-  //var navigationList = document.getElementById('navList');
+//alert('appear');
+//var navigationList = document.getElementById('navList');
 
-  //if (navigationList.style.display == 'none') {
-    navigationList.style.display = 'block';
-  //} else {
-    navigationList.style.display == 'none';
-  //}
+//if (navigationList.style.display == 'none') {
+navigationList.style.display = 'block';
+//} else {
+navigationList.style.display == 'none';
+//}
 //}
